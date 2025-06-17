8cf3d18f5066889e74b71cea7227b7ef9cf9fe96
Emmanuel Valverde Ramos <emmanuel.valverde@volkswagen-groupservices.com>
2025-06-17 07:32:42 +0200

    chore: basic setup

--- kotlin/bin/test/ApplicationSpec.kt
+++ kotlin/bin/test/ApplicationSpec.kt
@@ -1,9 +1,25 @@
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
-class ApplicationSpec :
-        StringSpec({
-            "it should fail" { true shouldBe false }
+class ApplicationTest {
 
-            "it should succeed" { true shouldBe true }
-        })
+    @Test
+    fun itShouldFail() {
+        assertTrue(false) // test intentionally fails like `true shouldBe false`
+    }
+
+    @Test
+    fun itShouldSucceed() {
+        assertTrue(true)
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = [true, false])
+    fun paramTest(value: Boolean) {
+        println("Running with: $value")
+        assertTrue(value || !value) // dummy assertion: always true
+    }
+}
---
+++ kotlin/src/test/kotlin/ApplicationShould.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class ApplicationShould {
+
+    @Test
+    fun `a random name`() {
+        assertTrue(true)
+    }
+}
